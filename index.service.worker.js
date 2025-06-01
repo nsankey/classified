35940eaaba26dfb37b492b662117a57d8972494f
Niles Sankey <nilessankey@nyanheroes.com>
2025-06-01 10:23:58 +0900

    added logo, color updates #2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748740688|705695003';
+const CACHE_VERSION = '1748741017|1034520753';
 /** @type {string} */
 const CACHE_PREFIX = 'TestGodotMobile-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
