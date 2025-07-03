31c144634b25f9f28554fc8503953538b6734cad
Niles Sankey <nilessankey@nyanheroes.com>
2025-07-04 07:55:46 +0900

    Update/refresh

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748816169|161400168';
+const CACHE_VERSION = '1751583105|53653680';
 /** @type {string} */
 const CACHE_PREFIX = 'RazorformDatabas-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
