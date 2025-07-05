a2f64e3874ef9ac05f7f11dbe7e47c279c638f34
Niles Sankey <nilessankey@nyanheroes.com>
2025-07-06 07:22:08 +0900

    timing updates

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751753426|3312136744';
+const CACHE_VERSION = '1751754093|3979217195';
 /** @type {string} */
 const CACHE_PREFIX = 'RazorformDatabas-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
